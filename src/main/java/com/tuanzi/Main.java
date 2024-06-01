43f49da06a146d7fc3f63865c062cfa470488f43
tuanzi <1581291850@qq.com>
2024-06-01 17:02:14 +0800

    test

---
+++ src/main/java/com/tuanzi/Main.java
@@ -0,0 +1,9 @@
+package com.tuanzi;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("git");
+        System.out.println("44444444");
+    }
+}
